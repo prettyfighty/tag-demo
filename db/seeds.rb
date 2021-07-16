7cb6b0aed36f87ca5f340b38ffb8786b8927a431
lailai <prettyfighty@gmail.com>
2021-07-16 11:47:30 +0800

    create products through seed with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Product.create(name: Faker::Food.fruits, description: Faker::Food.description, price: Faker::Number.within(range: 1..999999))
+end
+
+5.times do
+  Product.create(name: Faker::Food.vegetables, description: Faker::Food.description, price: Faker::Number.within(range: 1..999999))
+end
+
+5.times do
+  Product.create(name: Faker::Food.sushi, description: Faker::Food.description, price: Faker::Number.within(range: 1..999999))
+end
+
+5.times do
+  Product.create(name: Faker::Food.dish, description: Faker::Food.description, price: Faker::Number.within(range: 1..999999))
+end
