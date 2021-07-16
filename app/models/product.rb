db47fa1e23c4358a8e4efe0f41f4c699a1f990ea
lailai <prettyfighty@gmail.com>
2021-07-16 14:02:15 +0800

    build relations (tag & product)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
 
   validates :name, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_030810) do
+ActiveRecord::Schema.define(version: 2021_07_16_053545) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -20,4 +20,21 @@ ActiveRecord::Schema.define(version: 2021_07_16_030810) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_taggings_on_product_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "taggings", "products"
+  add_foreign_key "taggings", "tags"
 end
